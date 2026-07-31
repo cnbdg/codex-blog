324093c59b73060f78b8ccf04625402bf10e255c
Cnbdg <xsfcnbdg@gmail.com>
2026-07-31 13:19:34 +0800

    Configure Supabase authentication project

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Supabase 项目的公开配置。Publishable key 可以安全地出现在浏览器前端。
 // 不要在这里填写 service_role key。
 window.BLOG_CONFIG = {
-  supabaseUrl: "",
-  supabasePublishableKey: ""
+  supabaseUrl: "https://kwewqbkyrsvvwywhzguy.supabase.co",
+  supabasePublishableKey: "sb_publishable_SIa_X98kBnD-dMos6JMTpA_H7LDdjg1"
 };
